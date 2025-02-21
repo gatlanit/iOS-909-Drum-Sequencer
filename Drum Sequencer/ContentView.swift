d616df23bfc28e2e96529f26b0be2d6f3ac0923c
Nitin Gatla <gatlanit@gmail.com>
2025-02-21 17:19:18 -0600

    Initial Commit

---
+++ Drum Sequencer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Drum Sequencer
+//
+//  Created by Nitin G on 2/21/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Drum Sequencer/Drum_SequencerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Drum_SequencerApp.swift
+//  Drum Sequencer
+//
+//  Created by Nitin G on 2/21/25.
+//
+
+import SwiftUI
+
+@main
+struct Drum_SequencerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
